fb4c30bfd2b3db0792d6954034a8dbe54be34c57
nikitin712 <amehanik1@mail.ru>
2025-06-21 14:01:58 +0300

    version1_select_mode

---
+++ DB_data/new_create.sql
@@ -0,0 +1,190 @@
+-- drop
+BEGIN;
+DROP TABLE IF EXISTS public."Cases_Bodies";
+DROP TABLE IF EXISTS public."Cases";
+DROP TABLE IF EXISTS public."Transactions";
+DROP TABLE IF EXISTS public."Accounts";
+DROP TABLE IF EXISTS public."Bodies";
+DROP TABLE IF EXISTS public."Persons";
+DROP TABLE IF EXISTS public."Cities";
+END;
+
+--create
+BEGIN;
+
+
+CREATE TABLE IF NOT EXISTS public."Persons"
+(
+    person_id integer NOT NULL,
+    first_name character varying(32) NOT NULL,
+    last_name character varying(32) NOT NULL,
+    second_name character varying(32),
+    birthday date NOT NULL,
+    current_city_id integer,
+    "INN" character varying(12) NOT NULL DEFAULT '000000000000',
+    PRIMARY KEY (person_id)
+);
+
+CREATE TABLE IF NOT EXISTS public."Cities"
+(
+    city_id integer NOT NULL,
+    city_name character varying(30) NOT NULL,
+    PRIMARY KEY (city_id)
+);
+
+CREATE TABLE IF NOT EXISTS public."Accounts"
+(
+    account_id integer NOT NULL,
+    account_number character varying(20) NOT NULL DEFAULT '0000000000000000',
+    person_id integer,
+    body_id integer,
+    open_date timestamp without time zone,
+    close_date timestamp without time zone,
+    PRIMARY KEY (account_id)
+);
+
+CREATE TABLE IF NOT EXISTS public."Bodies"
+(
+    body_id integer NOT NULL,
+    body_type character varying(3) NOT NULL,
+    "INN" character varying(12) NOT NULL,
+    body_name character varying(100),
+    start_date date,
+	end_date date,
+    "CEO_id_or_person_id" integer NOT NULL,
+    PRIMARY KEY (body_id)
+);
+
+CREATE TABLE IF NOT EXISTS public."Transactions"
+(
+    trans_id integer NOT NULL,
+    acc_from integer NOT NULL,
+    acc_to integer NOT NULL,
+	trans_date date NOT NULL,
+    amount numeric(20, 2) NOT NULL,
+    description character varying(100),
+    PRIMARY KEY (trans_id)
+);
+
+CREATE TABLE IF NOT EXISTS public."Cases"
+(
+    case_id integer NOT NULL,
+    start_date date NOT NULL,
+    end_date date,
+    description character varying(100),
+    law_number character varying(30),
+    PRIMARY KEY (case_id)
+);
+
+CREATE TABLE IF NOT EXISTS public."Cases_Bodies"
+(
+    case_id integer NOT NULL,
+    body_id integer NOT NULL,
+    PRIMARY KEY (case_id, body_id)
+);
+
+ALTER TABLE IF EXISTS public."Persons"
+    ADD FOREIGN KEY (current_city_id)
+    REFERENCES public."Cities" (city_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."Accounts"
+    ADD FOREIGN KEY (person_id)
+    REFERENCES public."Persons" (person_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."Accounts"
+    ADD FOREIGN KEY (body_id)
+    REFERENCES public."Bodies" (body_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."Bodies"
+    ADD FOREIGN KEY ("CEO_id_or_person_id")
+    REFERENCES public."Persons" (person_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."Transactions"
+    ADD FOREIGN KEY (acc_from)
+    REFERENCES public."Accounts" (account_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."Transactions"
+    ADD FOREIGN KEY (acc_to)
+    REFERENCES public."Accounts" (account_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."Cases_Bodies"
+    ADD FOREIGN KEY (case_id)
+    REFERENCES public."Cases" (case_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."Cases_Bodies"
+    ADD FOREIGN KEY (body_id)
+    REFERENCES public."Bodies" (body_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+    NOT VALID;
+
+END;
+
+--insert
+BEGIN;
+INSERT INTO public."Cities" VALUES
+(1, 'Moscow'),
+(2, 'St. Petersberg'),
+(3, 'Sochi'),
+(4, 'Ekaterinburg'),
+(5, 'Vladimir'),
+(6, 'Kazan'),
+(7, 'Krasnogorsk'),
+(8, 'Nizhniy Novgorod'),
+(9, 'Samara'),
+(10, 'Chelyabinsk');
+
+INSERT INTO public."Persons" VALUES
+(1, 'Blinovskaya', 'Elena', 'Olegovna', '1981-08-25', 1, '760303376240'),
+(2, 'Vasiliev', 'Nikolay', 'Vladimirovich', '2000-01-01', 4, '123456789010'),
+(3, 'Kravcev', 'Ivan', 'Borisovich', '1995-04-25', 9, '109876543210'),
+(4, 'Strelnikova', 'Yula', 'Grigorevna', '1972-11-11', 5, '111122223333'),
+(5, 'Levushkin', 'Marat', 'Yusupovich', '1980-03-17', 1, '444455556666'),
+(6, 'Trolin', 'Viktor', NULL, '1985-06-07', 1, '777788889999');
+
+INSERT INTO public."Bodies" VALUES
+(1, 'ENT', '111222333444', 'Vasilek', '2015-06-09', Null, 3),
+(2, 'OOO', '5555566666', 'MetallNew', '2003-02-26', Null, 1),
+(3, 'OAO', '7777788888', 'Gorgeus', '2011-11-11', '2025-03-02', 2),
+(4, 'GUP', '3434343434', 'MetallOld', '2010-10-19', Null, 1),
+(5, 'ZAO', '1010101010', 'Kommercia', '2023-09-10', Null, 6),
+(6, 'OOO', '1234512345', 'TransSib', '2024-01-02', Null, 2),
+(7, 'fiz', '760303376240', Null, Null, Null, 1),
+(8, 'fiz', '123456789010', Null, Null, Null, 2),
+(9, 'fiz', '109876543210', Null, Null, Null, 3),
+(10, 'fiz', '111122223333', Null, Null, Null, 4),
+(11, 'fiz', '444455556666', Null, Null, Null, 5),
+(12, 'fiz', '777788889999', Null, Null, Null, 6);
+
+END;
+
+
+-- ROLLBACK;
